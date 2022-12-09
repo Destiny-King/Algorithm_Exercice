62cabdbd0c4e31cadc156b3ec3f00356043ad28a
Destiny-King <2681283464@qq.com>
2022-12-09 09:12:48 +0800

    Add ClimbStairs2.

---
+++ DynamicProgramming/ClimbStairs2/ClimbStairs2.h
@@ -0,0 +1,17 @@
+//
+// Created by Destiny on 2022/12/9.
+//
+
+#ifndef DYNAMICPROGRAMMING_CLIMBSTAIRS2_H
+#define DYNAMICPROGRAMMING_CLIMBSTAIRS2_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class ClimbStairs2 {
+public:
+  int climbStairs2(int n);
+};
+
+#endif // DYNAMICPROGRAMMING_CLIMBSTAIRS2_H
---
+++ DynamicProgramming/ClimbStairs2/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Destiny on 2022/12/9.
+//
+#include "ClimbStairs2.h"
+
+int main() {
+  system("chcp 65001");
+
+  ClimbStairs2 climbStairs2;
+
+  int n = 3;
+  int ret = climbStairs2.climbStairs2(n);
+  cout << ret << endl;
+
+  return 0;
+}
