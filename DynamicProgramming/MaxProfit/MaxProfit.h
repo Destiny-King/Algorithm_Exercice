fe83623fba1cf158ec2c1c73bcddf55f86e4cba2
Destiny-King <2681283464@qq.com>
2022-12-28 09:43:37 +0800

    Add MaxProfit.

--- DynamicProgramming/MaxProfit/MaxProfit.h
+++ DynamicProgramming/MaxProfit/MaxProfit.h
@@ -15,7 +15,8 @@ public:
   int maxProfit2(vector<int> &prices);
   int maxProfit3(vector<int> &prices);
   int maxProfit4(int k, vector<int> &prices);
-  int maxProfit5(vector<int> &prices); //含冰冻期
+  int maxProfit5(vector<int> &prices);          //含冰冻期
+  int maxProfit6(vector<int> &prices, int fee); //含手续费
 };
 
 #endif // DYNAMICPROGRAMMING_MAXPROFIT_H
--- DynamicProgramming/MaxProfit/main.cpp
+++ DynamicProgramming/MaxProfit/main.cpp
@@ -8,7 +8,7 @@ int main() {
 
   MaxProfit maxProfit;
 
-  vector<int> cost = {1, 2, 3, 0, 2};
+  vector<int> cost = {1, 3, 2, 8, 4, 9};
   int ret = maxProfit.maxProfit(cost);
   cout << ret << endl;
 
@@ -24,6 +24,10 @@ int main() {
 
   int ret5 = maxProfit.maxProfit5(cost);
   cout << ret5 << endl;
+
+  int fee = 2;
+  int ret6 = maxProfit.maxProfit6(cost, fee);
+  cout << ret6 << endl;
 
   return 0;
 }
